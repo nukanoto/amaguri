ff27543685d9dc122d86fc0c71e18f7a22c31eac
nukanoto <me@nukanoto.net>
2025-10-15 19:14:08 +0900

    read latest mail with imap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use anyhow::Result;
+use native_tls::TlsConnector;
+
+struct Config {
+    imap_host: String,
+    imap_port: u16,
+    imap_domain: String,
+    imap_username: String,
+    imap_password: String,
+    discord_webhook_url: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let config = Config {
+        imap_host: env::var("IMAP_HOST").expect("IMAP_HOST must be set"),
+        imap_port: env::var("IMAP_PORT")
+            .unwrap_or_default()
+            .parse::<u16>()
+            .unwrap_or(993),
+        imap_domain: env::var("IMAP_DOMAIN").expect("IMAP_DOMAIN must be set"),
+        imap_username: env::var("IMAP_USERNAME").expect("IMAP_USERNAME must be set"),
+        imap_password: env::var("IMAP_PASSWORD").expect("IMAP_PASSWORD must be set"),
+        discord_webhook_url: env::var("DISCORD_WEBHOOK_URL")
+            .expect("DISCORD_WEBHOOK_URL must be set"),
+    };
+    let tls_connector = TlsConnector::builder().build()?;
+
+    let client = imap::connect(
+        (config.imap_host, config.imap_port),
+        config.imap_domain,
+        &tls_connector,
+    )?;
+
+    // the client we have here is unauthenticated.
+    // to do anything useful with the e-mails, we need to log in
+    let mut imap_session = client
+        .login(config.imap_username, config.imap_password)
+        .map_err(|e| e.0)?;
+
+    // we want to fetch the first email in the INBOX mailbox
+    imap_session.select("INBOX")?;
+
+    // fetch message number 1 in this mailbox, along with its RFC822 field.
+    // RFC 822 dictates the format of the body of e-mails
+    let messages = imap_session.fetch("1", "RFC822")?;
+    let message = if let Some(m) = messages.iter().next() {
+        m
+    } else {
+        println!("INBOX にメールが見つかりませんでした。");
+        return Ok(());
+    };
+
+    // extract the message's body
+    let body = message.body().expect("message did not have a body!");
+    let body = std::str::from_utf8(body)
+        .expect("message was not valid utf-8")
+        .to_string();
+
+    println!("{body}");
+
+    // be nice to the server and log out
+    imap_session.logout()?;
+
+    Ok(())
 }
