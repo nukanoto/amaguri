60673cc9b751f4a831315bdda59fd140a4857c95
nukanoto <me@nukanoto.net>
2025-10-15 17:44:12 +0900

    cargo init

--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const STATE_KEY = "imap_state_v1";
-export const MAX_HASH_HISTORY = 2000;
--- src/utils/hash.ts
+++
@@ -1,8 +0,0 @@
-const encoder = new TextEncoder();
-
-export async function hashString(value: string): Promise<string> {
-  const digest = await crypto.subtle.digest("SHA-256", encoder.encode(value));
-  return Array.from(new Uint8Array(digest))
-    .map((byte) => byte.toString(16).padStart(2, "0"))
-    .join("");
-}
